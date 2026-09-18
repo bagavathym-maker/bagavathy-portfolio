f90fb9a4667a8107a769cf89a48574efc31287d8
bagavathy M <bagavathysri1@gmail.com>
2026-09-19 00:09:28 +0530

    Delete backend directory

--- backend/middleware/errorHandler.js
+++
@@ -1,1 +0,0 @@
-function errorHandler(err,req,res,next){console.error(err);res.status(err.status||500).json({success:false,message:err.status?err.message:"Something went wrong"});} module.exports=errorHandler;
--- backend/routes/experience.js
+++
@@ -1,3 +0,0 @@
-const router=require("express").Router();const db=require("../config/db");
-router.get("/",async(req,res,next)=>{try{const [rows]=await db.query("SELECT * FROM experiences ORDER BY start_date DESC, created_at DESC");res.json({success:true,data:rows})}catch(e){next(e)}});
-module.exports=router;
